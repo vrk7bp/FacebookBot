e0a783703f91aa982a707bc45855f2132b9a5063
vrk7bp <vrk7bp@virginia.edu>
2016-10-02 04:47:32 +0000

    Changing views template for flask framework

--- constants.py
+++ constants.py
@@ -1,5 +1,2 @@
-#Empty file to define your constants. Have empty strings for constants that I used, but more can be added.
-
-MY_TOKEN = ""
+MY_TOKEN = "token"
 VERIFY_TOKEN = ""
-
--- views.py
+++ views.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from flask import render_template
 from flask import redirect
 from flask import request
+
+import requests
+import json
+import constants
+
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
@@ -10,27 +15,60 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "this_is_a_test":
+        if not request.args.get("hub.verify_token") == constants.VERIFY_TOKEN:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
 
+@application.route('/', methods=['POST'])
+def webhook():
 
+    # endpoint for processing incoming messaging events
 
-@application.route('/home')
-def home():
-	return "<h1 style='color:blue'>Hello There!</h1>"
+    data = request.get_json()
 
-@application.errorhandler(404)
-def page_not_found(e):
-    """Custom 404 Page."""
-    return "<h1 style='color:red'>404 Error :(</h1>", 404
+    if data["object"] == "page":
 
-@application.errorhandler(500)
-def page_not_found(e):
-    """Custom 500 Page."""
-    return "<h1 style='color:red'>500 Error :(</h1>", 500
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "got it, thanks!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+def send_message(recipient_id, message_text):
+
+    params = {
+        "access_token": constants.MY_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
